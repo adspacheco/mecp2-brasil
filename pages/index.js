c73780a6a851a4b86dfe0ca3be8d28609ef7973d
Anderson Pacheco <39010933+adspacheco@users.noreply.github.com>
2023-03-10 01:37:39 +0000

    create index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,51 @@
 function Home() {
-    return <h1>mecp2 Brasil</h1>
+    return <div>
+        <h1>MECP2 Brasil</h1>
+        <p>
+            Olá, sou o Anderson, marido da Camila e pai do Gabriel, diagnosticado com síndrome de duplicação MECP2. 
+        </p>
+        <p>
+            Causada pela duplicação do gene MECP2, essa síndrome é um grave distúrbio do desenvolvimento neurológico. 
+        </p>
+        <p>
+            Por ser uma doença rara, é difícil encontrar material em português. No Brasil, conheço apenas outros 2 casos.
+        </p>
+        <p>
+            Assim, todas as informações que possam ser úteis disponibilizarei nessa página.
+        </p>
+        <p>
+            Começando com alguns conselhos que peguei no site do <a href="https://nri.texaschildrens.org/sites/nri/files/Vinson-MecP2DupParentTips2020.pdf">Texas Children's Hospital</a> e algumas experiências pessoais!
+        </p>
+        <h2>Conselhos para pais de crianças com MECP2</h2>
+        <ul>
+            <li><p>Divirta-se com seu filho diariamente</p></li>
+            <li><p>Comemore pequenas conquistas e não faça comparações com outras crianças </p></li>
+            <li><p>Cuide de si mesmo e dos seus relacionamentos</p></li>
+            <li><p>Tenha consciência do diagnóstico, mas não faça dele o centro da sua vida</p></li>
+            <li><p>Seu filho possui a mesma necessidade de amor que qualquer outra criança</p></li>
+            <li><p>As intervenções ABA costumam trazer maiores ganhos (com ou sem autismo)</p></li>
+            <li><p>Procure sobre as terapias cobertas pelo seu plano de saúde</p></li>
+            <li><p>Alguns planos possuem programas específicos de apoio</p></li>
+            <li><p>Não comprometa suas finanças, caso não tenha plano de saúde ou cobertura de terapias</p></li>
+            <li><p>Todos podem aprender coisas novas</p></li>
+            <li><p>Aprenda com professores e terapeutas e estimule seu filho em casa nas situações do cotidiano</p></li>
+            <li><p>Seja parceiro dos médicos e terapeutas</p></li>
+            <li><p>5 min. de prática todo dia é melhor que 1h na semana inteira</p></li>
+            <li><p>Divida uma instrução em pequenos passos</p></li>
+            <li><p>Conheça as leis federais e estaduais relacionadas à PCD</p></li>
+            <li><p>Esteja preparado para situações difíceis, mas busque e ore pelo melhor</p></li>
+            <li><p>Seu filho tem a mesma capacidade trazer amor e alegria que qualquer outra criança</p></li>
+            <li><p>Procure por hospitais públicos de referência em doenças raras</p></li>
+            <li><p>Autolesão e gritos podem estar relacionados à falta do que fazer, dor ou ansiedade</p></li>
+            <li><p>Você tem direito a sacar o FGTS para ajudar nas despesas familiares</p></li>
+            <li><p>Reconheça seu filho por caracterísitcas únicas, não apenas pelo MECP2</p></li>
+            <li><p>Pergunte ao médico sobre segurança e eficácia dos tratamentos prescritos</p></li>
+            <li><p>Procure ter uma rotina organizada, evitando surpresas quando a criança estiver cansada ou com fome</p></li>
+            <li><p>DIAGNÓSTICO NÃO É DESTINO!</p></li>
+        </ul>
+        <h4>Precisando de ajuda? <a href="mailto:adspacheco07@gmail.com">Clique aqui</a> e me mande um e-mail!</h4>
+        <p>Última atualização da página em 09-03-2023 às 22:37</p>
+    </div>
 }
 
 export default Home;
