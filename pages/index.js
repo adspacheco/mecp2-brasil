889c71efc92d3bcb85eb12b4b5da603fb88edf3b
adspacheco <andersonac7@outlook.com>
2024-01-16 10:25:49 -0300

    Add _document.js and update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import Image from "next/image";
+
 function Home() {
-  return <h1 className="text-3xl font-bold underline">MECP2 Brasil</h1>;
+  return (
+    <main className="flex flex-col items-center justify-center h-screen gap-10">
+      <Image src="./logo.svg" width={220} height={80} />
+      <div className="w-[30rem]">
+        <h2 className="text-xl text-center">
+          Suporte e informações para famílias de pessoas com a Síndrome de
+          Duplicação MECP2
+        </h2>
+      </div>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        <div className="border p-8 flex flex-col rounded-lg">
+          <h3 className="text-xl text-center">Para pais</h3>
+        </div>
+        <div className="border p-8 flex flex-col rounded-lg">
+          <h3 className="text-xl text-center">Para médicos</h3>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
